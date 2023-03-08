500562965d282fa5a5ea6ac81397561b49c0ddec
Yaney Alvarado <yaneyalvarado@Yaneys-MacBook-Pro.local>
2023-03-07 17:52:32 -0700

    added code to JavaScript

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # PasswordLock
 JavaScript Challenge: Password Generator
+
+## Description
+For all employees with access to sensitive data, I have created a password generator that meets certain criteria(s). In this case, all employees will be able to create highly strong passwords that provides
+greater security for all. 
+
+
+## Instructions
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+## Mock Up
+
+## Deployment
+
+## GitHub
--- script.js
+++ script.js
@@ -1,8 +1,58 @@
 // Assignment code here
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var number = "123456789"
+var specialCharacters = "!@#$%*()^&<>?/:;+=|"
 
 
+function charactergenerator(str) {
+  return str[Math.floor(Math.random() * str.length)]
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  var length; 
+  var password = ""
+  var passwordoptions = ""
+
+
+  while (length < 8 || length > 128 || isNaN(length)) {
+    length = parseInt(prompt("How long would you like your password to be?"))
+  }
+
+  var includelowercase = confirm("Do you want your password to contain lowercase letters?")
+  var includeuppercase = confirm("Do you want your password to contain uppercase letters?")
+  var includenumbers = confirm("Do you want your password to contain numbers?")
+  var includespecialcharacters = confirm("Do you want your password to contain special characters?")
+
+  if (includelowercase) {
+    password += charactergenerator(lowercase)
+    passwordoptions += lowercase
+  }
+
+  if (includeuppercase) {
+    password += charactergenerator(uppercase)
+    passwordoptions += uppercase
+  }
+
+  if (includenumbers) {
+    password += charactergenerator(number)
+    passwordoptions += number
+  }
+
+  if (includespecialcharacters) {
+    password += charactergenerator(specialCharacters)
+    passwordoptions += specialCharacters
+  }
+
+  for (let i = password.length; i < length; i++) {
+    password += charactergenerator(passwordoptions)
+    
+  }
+
+  return password 
+}
+
 
 // Write password to the #password input
 function writePassword() {
